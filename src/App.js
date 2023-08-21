dd92dc0e2f99078d2ac029b65031e2e4480cfe6f
Shashwat Pal <shashwatpal1021@gmail.com>
2023-08-21 15:23:01 +0530

    added a project card-dispaly through api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import Loading from "./Loading";
+import { BsGithub } from "react-icons/bs";
+import "./styles.css";
+import "./navbar.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleFetch = () => {
+    setIsLoading(true);
+    fetch("https://reqres.in/api/users?page=0")
+      .then((respose) => respose.json())
+      .then((respose) => {
+        setUsers(respose.data);
+        setIsLoading(false);
+        setTimeout(() => {
+          setUsers(respose.data);
+          setIsLoading(false);
+        }, 2000);
+      }).catch(() => {
+        setErrorMessage("Unable to fetch user list");
+        setIsLoading(false);
+      });
+  };
+  const renderUser = (
+    <div className="userlist-container">
+      {users.map((item, index) => (
+        <div className="user-container" key={index}>
+          <img src={item.avatar} alt="" />
+          <div className="userDetail">
+            <div className="first-name">
+              {`${item.first_name}                
+                ${item.last_name}`}
+            </div>
+            <div className="last-name">{item.email}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
+  return (
+    <>
+      <nav className="navbar">
+        <h1>Brand Name</h1>
+      </nav>
+      <div className="App">
+        <button onClick={handleFetch} disabled={isLoading}>
+          Fetch Users
+        </button>
+        {isLoading ? <Loading /> : renderUser}
+        {errorMessage && <div className="error">{errorMessage}</div>}
+      </div>
+    </>
+  );
 }
-
-export default App;
